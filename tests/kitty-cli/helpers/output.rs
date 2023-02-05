0b95e96d6d941beac80fc43416c22b45e705449d
Adrian Borup <adrian@borup.com>
2023-02-05 12:46:06 +0100

    Remove unnecessary lines

--- tests/kitty-cli/helpers/output.rs
+++ tests/kitty-cli/helpers/output.rs
@@ -5,9 +5,6 @@ pub struct Output {
 }
 
 pub fn assert_output_eq(actual: impl AsRef<str>, expected: impl AsRef<str>) {
-    let actual = actual.as_ref();
-    let expected = expected.as_ref();
-
     assert_eq!(normalise_string(actual), normalise_string(expected),);
 }
 
